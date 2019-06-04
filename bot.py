6a2706d405eeebb0c04f0c51e5ab8c0ec3ec6b72
arefnue <greatadamas@gmail.com>
2019-06-04 17:14:12 +0200

    Ver2 First commit

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+import time
+import asyncio
+import os
+from discord.ext import commands
+
+# Server informations
+# index 0=server_id, 1=token, 2=client_secret, 3=client_id
+server = []
+
+with open("server.txt", "rt") as myfile:
+    for myline in myfile:
+        server.append(myline.rstrip("\n"))
+
+serverID = int(server[0])
+token = server[1]
+
+
+bot = commands.Bot(command_prefix = '!')
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+bot.run(token)
